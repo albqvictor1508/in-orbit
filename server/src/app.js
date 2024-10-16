0f0e6c1471912a38fd3d3ce1ced072fbd2e39817
Victor Arruda <albq.victor@gmail.com>
2024-10-16 09:03:18 -0300

    commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 import "./db/index";
 import { resolve } from "node:path";
+import helmet from "helmet";
+import cors from "cors";
 
 import express from "express";
 import TokenRoutes from "./routes/TokenRoutes";
@@ -7,6 +9,18 @@ import UserRoutes from "./routes/UserRoutes";
 import homeRoutes from "./routes/HomeRoutes";
 import AlunoRoutes from "./routes/AlunoRoutes";
 import PhotoRoutes from "./routes/PhotoRoutes";
+
+const whitelist = ["http://34.95.217.255:81/", "http://localhost:5173/"];
+
+const corsOptions = {
+	origin(origin, callback) {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			return callback(null, true);
+		}
+
+		return new Error("not allowed by CORS");
+	},
+};
 
 class App {
 	constructor() {
@@ -16,6 +30,8 @@ class App {
 	}
 
 	middlewares() {
+		this.app.use(cors(corsOptions));
+		this.app.use(helmet());
 		this.app.use(express.json());
 		this.app.use(express.urlencoded({ extended: true }));
 		this.app.use(express.static(resolve(__dirname, "uploads")));
