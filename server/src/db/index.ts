bdd73c9fa21f762a48b7979a2158e1e40701975f
albqvictor1508 <albq.victor@gmail.com>
2024-12-25 12:40:46 -0300

    tirei a validação do env

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,9 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
-import { env } from '../env'
 
-export const client = postgres(env.DATABASE_URL)
+export const client = postgres(process.env.DATABASE_URL as string)
 export const db = drizzle(client, {
   schema,
   logger: false,
